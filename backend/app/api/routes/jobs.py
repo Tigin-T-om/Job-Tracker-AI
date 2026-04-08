f9172da7be7a5d8ce67f025e4e7b7796d981be00
Tigin-T-om <tigintom158@gmail.com>
2026-04-08 09:44:46 +0530

    Add update and delete job API's

--- backend/app/api/routes/jobs.py
+++ backend/app/api/routes/jobs.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.models.job import Job
-from app.schemas.job import JobCreate, JobResponse
+from app.schemas.job import JobCreate, JobUpdate, JobResponse
 
 router = APIRouter()
 
@@ -27,3 +27,33 @@ def create_job(job: JobCreate, db: Session = Depends(get_db)):
 def get_jobs(db:Session = Depends(get_db)):
     jobs = db.query(Job).all()
     return jobs
+
+@router.put("/{job_id}", response_model=JobResponse)
+def update_job(job_id: int, updated_job: JobUpdate, db: Session = Depends(get_db)):
+    job = db.query(Job).filter(Job.id == job_id).first()
+
+    if not job:
+        raise HTTPException(status_code=404, detail="Job not found")
+    
+    job.company_name = updated_job.company_name
+    job.role = updated_job.role
+    job.job_link = updated_job.job_link
+    job.location = updated_job.location
+    job.status = updated_job.status
+
+    db.commit()
+    db.refresh(job)
+
+    return job
+
+@router.delete("/{job_id}")
+def delete_job(job_id: int, db: Session = Depends(get_db)):
+    job = db.query(Job).filter(Job.id == job_id).first()
+
+    if not job:
+        raise HTTPException(status_code=404, detail="Job not found")
+    
+    db.delete(job)
+    db.commit()
+
+    return {"message": "Job deleted successfully"}
--- backend/app/schemas/job.py
+++ backend/app/schemas/job.py
@@ -8,6 +8,13 @@ class JobCreate(BaseModel):
     location: Optional[str] = None
     status: str
 
+class JobUpdate(BaseModel):
+    company_name: str
+    role: str
+    job_link: Optional[str] = None
+    location: Optional[str] = None
+    status: str
+
 class JobResponse(BaseModel):
     id: int
     company_name: str
